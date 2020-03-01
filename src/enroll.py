f01b8051e0c08dbc4aa6a7c95b12e183808b91d2
awave1 <artgolovin96@gmail.com>
2020-03-01 01:36:39 -0700

    :sparkles: (add password validation)

--- src/enroll.py
+++ src/enroll.py
@@ -1,25 +1,75 @@
-# /usr/bin/env python3
-
-from argon2 import PasswordHasher
+from argon_hasher import ArgonHasher
 from db import Database
 import sys
 import argparse
+import re
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("username", type=str, help="Username")
-parser.add_argument("password", type=str, help="Password")
-args = parser.parse_args()
+def is_valid_password(password):
+    """
+    Check if given password is valid. A valid password cannot contain the following:
 
-db = Database()
+    - can't contain only numbers
+    - can't contain words form the given dictionary, pre- or post- fixed by a number
+    
+    Arguments:
+        password {str} -- password string
+    
+    Returns:
+        [bool] -- true, if password is valid, false otherwise
+    """
 
-ph = PasswordHasher()
-username = args.username
-password = ph.hash(args.password)
+    password = password.lower()
 
-if db.add_user(username, password):
-    print("accepted")
-    sys.exit(0)
-else:
-    print("rejected")
-    sys.exit(-1)
+    # check if password contains only numbers
+    if password.isdigit():
+        return False
+
+    with open("data/words") as words:
+        # remove all numbers from the password
+        password = "".join(list(filter(lambda c: c.isalpha(), password)))
+
+        # read the dictionary words from the file and keep any matches with specified password
+        dictionary_words = list(
+            filter(
+                lambda w: w == password,
+                map(lambda w: w.lower(), words.read().split("\n")),
+            )
+        )
+
+        # if dictionary_words contain any elements, return False
+        if len(dictionary_words) != 0:
+            return False
+
+    return True
+
+
+def enroll(username, password):
+    db = Database()
+    argon2 = ArgonHasher()
+
+    def accept_state():
+        print("accepted")
+        sys.exit(0)
+
+    def reject_state():
+        print("rejected")
+        sys.exit(-1)
+
+    if not is_valid_password(password):
+        reject_state()
+
+    password_hash = argon2.hash(password)
+    if db.add_user(username, password_hash):
+        accept_state()
+    else:
+        reject_state()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("username", type=str, help="Username")
+    parser.add_argument("password", type=str, help="Password")
+    args = parser.parse_args()
+
+    enroll(args.username, args.password)
